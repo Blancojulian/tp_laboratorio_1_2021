3fa2e8f6e1fa808d1a696363ea4b206c6b35d5f7
Blancojulian <julianblanco963@hotmail.com>
2021-06-11 22:56:48 -0300

    listo

---
+++ TP_3/src/main.c
@@ -0,0 +1,84 @@
+/*
+ * menu.h
+ * Author: Blanco Julián Agustín
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "Controller.h"
+#include "Employee.h"
+#include "LinkedList.h"
+#include "menu.h"
+
+int main()
+{
+	setbuf(stdout,NULL);
+    int option = 0;
+    LinkedList* listaEmpleados = ll_newLinkedList();
+    do
+    {
+    	option = 0;
+    	utn_showMenu(&option, listaEmpleados);
+        switch(option)
+        {
+            case 1:
+                if(!(controller_loadFromText("data2.csv",listaEmpleados)))
+				{
+                	printf("Los datos se cargaron correctamente.\n");
+				}
+                break;
+            case 2:
+            	if(!(controller_loadFromBinary("data.bin",listaEmpleados)))
+				{
+					printf("Los datos se cargaron correctamente.\n");
+				}
+            	break;
+            case 3:
+            	if(!(controller_addEmployee(listaEmpleados)))
+				{
+					printf("El empleado se cargo correctamente.\n");
+				}
+            	break;
+            case 4:
+            	if(!(controller_editEmployee(listaEmpleados)))
+				{
+					printf("El empleado se modifico correctamente.\n");
+				}
+            	break;
+            case 5:
+            	if(!(controller_removeEmployee(listaEmpleados)))
+				{
+					printf("El empleado se elimino correctamente.\n");
+				}
+            	break;
+            case 6:
+            	if(!(controller_ListEmployee(listaEmpleados)))
+            	{
+            		printf("La lista se mostro correctamente.\n");
+            	}
+            	break;
+            case 7:
+            	if(!(controller_sortEmployee(listaEmpleados)))
+				{
+					printf("La lista se ordeno correctamente.\n");
+				}
+            	break;
+            case 8:
+            	if(!(controller_saveAsText("data2.csv", listaEmpleados)))
+				{
+					printf("Los datos se guardaron correctamente en el archivo de texto.\n");
+				}
+            	break;
+            case 9:
+            	if(!(controller_saveAsBinary("data.bin", listaEmpleados)))
+				{
+					printf("Los datos se guardaron correctamente en el archivo binario.\n");
+				}
+            	break;
+            case 10:
+            	printf("Adios.\n");
+            	break;
+        }
+    }while(option != OPTIONS);
+    return 0;
+}
+
---
+++ TP_3/src/menu.h
@@ -0,0 +1,13 @@
+/*
+ * menu.h
+ * Author: Blanco Julián Agustín
+ */
+
+#ifndef MENU_H_
+#define MENU_H_
+#define OPTIONS 10
+#include "LinkedList.h"
+
+int utn_showMenu(int* pOption, LinkedList* pArrayListEmployee);
+
+#endif /* MENU_H_ */
